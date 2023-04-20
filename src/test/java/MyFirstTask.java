4f4832cd6affa0295b04aec2e1f51f9d97481e1c
Ruslan Maksyutov <Ruslan.MAKSYUTOV@raiffeisen.ru>
2023-04-20 20:09:55 +0300

    Second task

--- src/test/java/MyFirstTask.java
+++ src/test/java/MyFirstTask.java
@@ -5,27 +5,42 @@ import org.openqa.selenium.Beta;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.naming.ldap.LdapReferralException;
+import java.io.File;
 import java.time.Duration;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTask {
 
-    private static WebDriver driver;
+    protected static WebDriver driver;
     private static WebDriverWait wait;
 
     @BeforeAll
     public static void start() {
+        //CHROME
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        //FIREFOX
+        //FirefoxOptions options = new FirefoxOptions();
+        //options.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe")));
+        //driver = new FirefoxDriver(options);
+
+        //IE
+        //driver = new InternetExplorerDriver();
+        //wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    @Test
     public void loginAdmin() {
-        driver.get("http://localhost/litecart/admin/");
+        driver.navigate().to("http://localhost/litecart/admin/");
+        //driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
---
+++ src/test/java/MySecondTask.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MySecondTask extends MyFirstTask{
+    private final String subMenuXPath = "//li[@class='selected']/ul/li";
+
+    @Test
+    public void investigateAdminPage() {
+        int menuSize = 0;
+        int subMenuSize = 0;
+        loginAdmin();
+        menuSize = driver.findElements(By.id("app-")).size();
+        for (int i = 0; i < menuSize; i++) {
+            driver.findElements(By.id("app-")).get(i).click();
+            System.out.println("Success!");
+            subMenuSize = driver.findElements(By.xpath(subMenuXPath)).size();
+            if (subMenuSize > 0) {
+                for (int j = 0; j < subMenuSize; j++) {
+                    driver.findElements(By.xpath(subMenuXPath)).get(j).click();
+                    List<WebElement> tagName = driver.findElements(By.tagName("h1"));
+                    assertTrue(tagName.size() > 0, "Tag h1 not exist on the page");
+                    System.out.println(tagName.get(0).getText() + " menu sub item clicked");
+                }
+            }
+        }
+
+    }
+
+}
